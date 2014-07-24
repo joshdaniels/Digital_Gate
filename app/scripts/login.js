aee4435dc519274ee4adbc3a5649a7bbf71978cc
Josh Daniels <daniels.joshua100@gmail.com>
2014-07-24 00:55:34 -0400

    got it

---
+++ app/scripts/login.js
@@ -0,0 +1,23 @@
+var User = Parse.Object.extend({
+  
+className: "user",
+
+  defaults:{
+ username: '',
+ password: ''
+  
+},
+  idAttribute: "objectId" ,
+
+  initialize: function (){
+    console.log("a new post has been created.");
+  }
+});
+
+var Users = Parse.Collection.extend({
+
+    model: User,
+   
+});
+
+var all_users = new Users();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('The Iron Yard Rocks');
+
+
+
+
+
+
+ $('.form1').on('submit', function  (event) {
+Parse.initialize("V1ZN54fnfyDsgUUzmbCWycnZIP7F9BV50HRmxrEK", "c4P7hEzLmJzuhTDld5yIovRV2bufaULxlfGjzq4x");
+
+
+event.preventDefault();
+
+var user = new Parse.User();
+user.set("username", $("#name").val());
+user.set("password", $("#password").val());
+user.set("email", $("#email").val());
+
+
+user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});
+
+$(this).trigger('reset');
+});
+
+
